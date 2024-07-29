4c28dcfcf305c10411fad9a6fed29975069a44c0
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-07-29 12:24:15 +0530

    server database connection done

--- Server/index.js
+++ Server/index.js
@@ -3,13 +3,23 @@ import dotenv from 'dotenv'
 dotenv.config();
 import cors from 'cors'
 import mongoose from 'mongoose';
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+try {
+    mongoose.connect(process.env.DB_URL)
+    console.log('Connected to MongoDB')
+}
+catch (err) {
+    console.error('Error connecting to MongoDB:', err)
+}
+
 app.get("/", (req, res) => {
     res.send("Hello, server is ready!");
 })
+
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
