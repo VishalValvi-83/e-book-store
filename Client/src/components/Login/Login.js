c65a241c9bcb811048d05a5ab130ddd6f1120869
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-07-30 01:50:09 +0530

    user authentication done

--- Client/src/components/Login/Login.js
+++ Client/src/components/Login/Login.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import toast from 'react-hot-toast'
+import axios from 'axios'
 function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const login = async () => {
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/login`, {
+                email: email,
+                password: password
+            })
+            if (response.data.message) {
+                toast.success(response.data.message)
+                localStorage.setItem('currentUser', JSON.stringify(response.data.data))
+                toast.loading("Redireting to Home")
+                setTimeout(() => {
+                    toast.dismiss()
+                    window.location.href = '/'
+                }, 2000)
+            }
+        } catch (err) {
+            toast.error("Invalid Credentials")
+            console.log("this error occur", err);
+        }
+    }
+
     return (
         <div>
             <div class="modal fade text-dark" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -13,14 +39,16 @@ function Login() {
                         <div class="modal-body">
                             <div className='form-group mb-3 p-2'>
                                 <label for="email">Email address</label>
-                                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
+                                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" value={email}
+                                    onChange={(e) => setEmail(e.target.value)} />
                             </div>
                             <div className='form-group p-2'>
                                 <label for="password">Password</label>
-                                <input type="password" class="form-control" id="password" placeholder="Password" />
+                                <input type="password" class="form-control" id="password" placeholder="Password" value={password}
+                                    onChange={(e) => setPassword(e.target.value)} />
                             </div>
                         </div>
-                        <button type="button" class="btn mb-2 my-0 mx-auto btn-warning">Login</button>
+                        <button type="button" class="btn mb-2 my-0 mx-auto btn-warning" onClick={login} >Login</button>
                         <div class="modal-footer ">
                             <p className='my-0 mx-auto'>Don't have an account? <a href="/signup">Signup</a></p>
                             <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
@@ -31,5 +59,4 @@ function Login() {
         </div>
     )
 }
-
 export default Login
